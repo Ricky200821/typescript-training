12577e05d1c6cf38ce5f5c991a345f5c02ce7022
Riccardo Trudu <riccardotrudu08@gmail.com>
2022-07-05 18:11:36 +0200

    versione 1.0

---
+++ src/calc copy.ts
@@ -0,0 +1,23 @@
+// node ./dist/calc.js 111 101 -
+
+import { calculate } from "./calculate-one-file"
+
+console.log('ricky"s binary calculator')
+
+const firstNumber = process.argv[2]
+const secondNumber = process.argv[3]
+const operation = process.argv[4]
+if (operation !== '+' && operation !== '-' && operation !== '*' && operation !== '/') {
+    console.error('operation not valid', operation)
+    process.exit(1)
+}
+const result = calculate(firstNumber, secondNumber, operation)
+if (result === undefined) {
+    console.error('one of the numbers is not binary')
+    process.exit(1)
+}
+
+console.log('first number', firstNumber);
+console.log('second number', secondNumber);
+console.log('operation', operation);
+console.log('the result is', result)
